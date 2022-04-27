de124d853855beabeca49ed820a42f15352ff80e
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-04-27 08:54:08 -0700

    added dissapear

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const hidingPlaces = ['tree', 'shed', 'boulder'];
 
 let correctGuesses = 0;
 let totalGuesses = 0;
+let losesGuesses = 0;
 
 shedButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
@@ -37,9 +38,23 @@ boulderButton.addEventListener('click', () => {
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
     // then increment the guesses
+    totalEl.textContent = totalGuesses += 1;
+    winsEl.textContent = correctGuesses += 1;
+    lossesEl.textContent = losesGuesses += 1;
     // then grab the appropriate container element for the correct guess from the DOM
+    hidingPlaces.textContent.add(hiding-places-face)
     // then add the face class to that element so that the face shows up
+    shedContainer.hidingPlaces.click('face');
+    treeContainer.hidingPlaces.click('face');
+    boulderContainer.hidingPlaces.click('face');
     // then if the user guess is correct, increment the correct guesses
+    total
+    .losesGuesses
+    .totalGuesses
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
 }
+
